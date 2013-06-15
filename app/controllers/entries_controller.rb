df73e720f14d555284bb9a404bf8a4a811aac1e5
Benjamin Smith <benjamin.lee.smith@gmail.com>
2013-06-15 14:37:46 -0600

    halt drawing entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,24 +1,27 @@
 class EntriesController < ApplicationController
   def create
-    entry = Entry.new(params[:entry])
-    if entry.save
-       render json: {
-           message: "You have a #{Entry.chance_to_win} chance to win an iPod Nano!"
-       }
-    else
-       if entry.errors[:email] == ["can't be blank"]
-         render status: 422, json: {
-             message: "Must provide email to enter!"
-         }
-       elsif entry.errors[:email] == ["has already been taken"]
-         render status: 422, json: {
-             message: "Only one entry allowed per email!"
-         }
-       else
-         render status: 418, json: {
-             message: "Ack! Something went wrong! Tell @benjamin_smith that something went wrong!"
-         }
-       end
-    end
+    render status: 422, json: {
+        message: "Sorry! Drawing is over!"
+    }
+    #entry = Entry.new(params[:entry])
+    #if entry.save
+    #   render json: {
+    #       message: "You have a #{Entry.chance_to_win} chance to win an iPod Nano!"
+    #   }
+    #else
+    #   if entry.errors[:email] == ["can't be blank"]
+    #     render status: 422, json: {
+    #         message: "Must provide email to enter!"
+    #     }
+    #   elsif entry.errors[:email] == ["has already been taken"]
+    #     render status: 422, json: {
+    #         message: "Only one entry allowed per email!"
+    #     }
+    #   else
+    #     render status: 418, json: {
+    #         message: "Ack! Something went wrong! Tell @benjamin_smith that something went wrong!"
+    #     }
+    #   end
+    #end
   end
 end
